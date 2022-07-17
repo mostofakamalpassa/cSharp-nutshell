5447b55d7cc5dfeef2c3c6fb35f54cb1af9e23b6
mkpbd <mostofakamal1990@gmail.com>
2022-07-17 18:19:02 +0600

    bootstriong

---
+++ CSharpInNutShell/Basic/UnitConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInNutShell.Basic
+{
+    internal class UnitConverter
+    {
+        int ratio; // Field  or variable or property
+
+        public UnitConverter(int unitRatio ) //  Constructor 
+        {
+            ratio = unitRatio;
+        }
+
+        public int Convert(int unit)
+        {
+            return ratio * unit;
+        }
+    }
+}
---
+++ CSharpInNutShell/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpInNutShell.Basic;
+
+//UnitConverter unitConverter = new UnitConverter(10);
+//Console.WriteLine(unitConverter.Convert(12) ); 
+//Console.WriteLine(unitConverter.Convert(30) ); 
