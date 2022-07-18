b671651ca0fe6ef08dd820960f82c1335ef13227
mkpbd <mostofakamal1990@gmail.com>
2022-07-18 15:38:45 +0600

    statement area|

--- CSharpInNutShell/Basic/Foo.cs
+++ CSharpInNutShell/Basic/Foo.cs
@@ -21,5 +21,62 @@ namespace CSharpInNutShell.Basic
          * void MyMethod (System.Text.StringBuilder sb) { ... } 
          * 
          */
+
+        public void NullCoalescingOperator()
+        {
+            string s1 = null;
+            string s2 = s1 ?? "nothing"; // s2 evaluates to "nothing"
+
+            /*
+             * If the lefthand expression is non-null, the righthand expression is never evaluated. The null-coalescing operator also works with nullable value types
+             */
+        }
+
+
+        public void NullCoalescingAssignmentOperator()
+        {
+
+            /*
+             * The ??= operator is the null-coalescing assignment operator.
+             * If the operand to the left is null, assign the right operand tothe left operand.
+             * 
+             */
+            string name = null;
+             name ??= "hello kamal";
+
+            // This is equivalent to:
+            if (name == null)
+            {
+                name = "Hello kamal";
+            }
+        }
+
+        public void NullConditionalOperator()
+        {
+            /*
+             * The ?. operator is the null-conditional or “Elvis” operator (after the Elvis emoticon). It allows you to call methods and access members just like the standard dot operator except that if the operand on the left is null, the expression evaluates to null instead of throwing a NullReferenceException:
+             * 
+             * 
+             * NullReferenceException:
+             */
+
+            System.Text.StringBuilder sb = null;
+            string s = sb?.ToString(); // No error; s instead evaluates to null
+
+            // The last line is equivalent to the following:
+            string ss = (sb == null ? null : sb.ToString());
+
+           // Null - conditional expressions also work with indexers:
+            string foo = null;
+            char? c = foo?[1]; // c is null
+
+            string x = null;
+            string y = null;
+            string z = "hello";
+
+            //x?.y?.z
+
+           // x == null ? null: (x.y == null ? null : x.y.z)
+        }
     }
 }
