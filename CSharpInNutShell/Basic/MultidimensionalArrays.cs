3dfc171752ebe0cb76e935cc378fa826cc218f3c
mkpbd <mostofakamal1990@gmail.com>
2022-07-18 13:30:34 +0600

    stack and heap Memory

--- CSharpInNutShell/Basic/MultidimensionalArrays.cs
+++ CSharpInNutShell/Basic/MultidimensionalArrays.cs
@@ -8,5 +8,119 @@ namespace CSharpInNutShell.Basic
 {
     internal class MultidimensionalArrays
     {
+
+        /*
+         * 
+         * Multidimensional arrays come in two varieties: rectangular and jagged. Rectangular arrays represent an n-dimensional block of memory, and jagged arrays are arrays of arrays.
+         * 
+         * 
+         */
+
+
+        public void RectangularArray()
+        {
+            /*
+             * Rectangular arrays are declared using commas to separate each dimension
+             */
+
+            int[,] matrix = new int[3, 3];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    matrix[i, j] = i * 3 + j;
+
+            /*
+             * You can initialize a rectangular array with explicit values.
+             */
+
+            int[,] matrixs = new int[,]{
+                                            {0,1,2},
+                                            {3,4,5},
+                                            {6,7,8}
+                                        };
+        }
+        public void JaggedArrays()
+        {
+
+            /*
+             * Jagged arrays are declared using successive square brackets to represent each dimension.
+             * 
+             */
+
+            int[][] matrix = new int[3][];
+
+            /*
+             * The inner dimensions aren’t specified in the declaration because, unlike a rectangular array, each inner array can be an arbitrary length. Each inner array is implicitly initialized to null rather than an empty array
+             * 
+             */
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new int[3]; // Create inner array
+                for (int j = 0; j < matrix[i].Length; j++)
+                    matrix[i][j] = i * 3 + j;
+            }
+
+
+            /*
+             * You can initialize a jagged array with explicit values
+             * 
+             */
+
+            int[][] matrixs = new int[][] {
+                                            new int[] {0,1,2},
+                                            new int[] {3,4,5},
+                                            new int[] {6,7,8,9}
+                                            };
+        }
+
+
+
+        public void ArrayInitializationExpressions()
+        {
+            /*
+             * There are two ways to shorten array initialization expressions. The first is to omit the new operator and type qualifications
+             * 
+             */
+
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            int[,] rectangularMatrix =
+                            {
+                            {0,1,2},
+                            {3,4,5},
+                            {6,7,8}
+                            };
+
+            int[][] jaggedMatrix =
+                                    {
+                                    new int[] {0,1,2},
+                                    new int[] {3,4,5},
+                                    new int[] {6,7,8,9}
+                                    };
+
+            /*
+             * The second approach is to use the var keyword, which instructs the compiler to implicitly type a local variable
+             * 
+             * 
+             */
+
+
+            var i = 3; // i is implicitly of type int
+            var s = "sausage"; // s is implicitly of type string
+                               // Therefore:
+            var rectMatrix = new int[,] // rectMatrix is implicitly of type int[,]
+                            {
+                                    {0,1,2},
+                                    {3,4,5},
+                                    {6,7,8}
+                            };
+            var jaggedMat = new int[][] // jaggedMat is implicitly of type int[][]
+                                        {
+                                        new int[] {0,1,2},
+                                        new int[] {3,4,5},
+                                        new int[] {6,7,8,9}
+                                        };
+        }
     }
 }
---
+++ CSharpInNutShell/Basic/VariableAndParameters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInNutShell.Basic
+{
+    internal class VariableAndParameters
+    {
+
+        /*
+         * A variable represents a storage location that has a modifiable value. A variable can be a local variable, parameter (value, ref, or out, or in), field (instance or static), or array element
+         * 
+         * 
+         */
+
+        /*
+         * The Stack and the Heap
+         * The stack and the heap are the places where variables reside. Each has very different lifetime semantics.
+         * 
+         */
+
+
+        public void Stack()
+        {
+            /*
+             * The stack is a block of memory for storing local variables and parameters.The stack logically grows and shrinks as a method or function is entered and exited
+             */
+
+
+        }
+
+        static int FactorialStack(int x)
+        {
+            if (x == 0) return 1;
+            return x * FactorialStack(x - 1);
+
+            /*
+             * This method is recursive, meaning that it calls itself. Each time the method is entered, a new int is allocated on the stack, and each time the method exits, the int is deallocated
+             */
+        }
+
+
+        public void HeapMemory()
+        {
+            /*
+             * The heap is the memory in which objects (i.e., reference-type instances) reside. Whenever a new object is created, it is allocated on the heap, and a reference to that object is returned. During a program’s execution, the heap begins filling up as new objects are created.
+             */
+
+            StringBuilder ref1 = new StringBuilder("object1");
+            Console.WriteLine(ref1);
+            // The StringBuilder referenced by ref1 is now eligible for GC.
+            StringBuilder ref2 = new StringBuilder("object2");
+            StringBuilder ref3 = ref2;
+            // The StringBuilder referenced by ref2 is NOT yet eligible for GC.
+            Console.WriteLine(ref3); // object2
+        }
+    }
+}
