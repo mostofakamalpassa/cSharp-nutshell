80cb4709e3f511dce6ac1f51c62618ee103f14dc
mkpbd <mostofakamal1990@gmail.com>
2022-07-18 14:49:46 +0600

    target type expression

---
+++ CSharpInNutShell/Basic/Foo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInNutShell.Basic
+{
+    internal class Foo
+    {
+        System.Text.StringBuilder sb;
+
+        public Foo(string initialize)
+        {
+            sb = new (initialize);
+        }
+
+        /*
+         * Target-typed new expressions are also helpful in the following scenario:
+         * MyMethod (new ("test"));
+         * void MyMethod (System.Text.StringBuilder sb) { ... } 
+         * 
+         */
+    }
+}
