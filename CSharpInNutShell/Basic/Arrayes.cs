8deb0d56cc18593d53293ae07eaeacd117223a07
mkpbd <mostofakamal1990@gmail.com>
2022-07-18 13:04:00 +0600

    Array range and array indices

--- CSharpInNutShell/Basic/Arrayes.cs
+++ CSharpInNutShell/Basic/Arrayes.cs
@@ -47,5 +47,57 @@ namespace CSharpInNutShell.Basic
              * All arrays inherit from the System.Array class, providing common services for all arrays. These members include methods to get and set elements regardless of the array type.
              */
         }
+
+
+        public void ArrayIndiceAndRange()
+        {
+
+            /*
+             * Indices and ranges working with elements or portions of an array.
+             * Indices
+             * Indices let you refer to elements relative to the end of an array, with the ^ operator. ^1 refers to the last element, ^2 refers to the second-to-last element, and so on
+             * 
+             * 
+             */
+
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            char lastElement = vowels[^1]; // 'u'
+            char secondToLast = vowels[^2]; // 'o'
+
+            /*
+             * implements indices with the help of the Index type, so you can also do the following
+             * 
+             */
+
+            Index first = 0;
+            Index last = ^1;
+            char firstElement1 = vowels[first]; // 'a'
+            char lastElement1 = vowels[last]; // 'u'
+        }
+
+
+        public void ArrayRange()
+        {
+
+            /*
+             * Ranges
+             * Ranges let you “slice” an array by using the .. operator
+             */
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            char[] firstTwo = vowels[..2]; // 'a', 'e'
+            char[] lastThree = vowels[2..]; // 'i', 'o', 'u'
+            char[] middleOne = vowels[2..3]; // 'i'
+
+            /*
+             * The second number in the range is exclusive, so ..2 returns the elements before vowels[2].
+             * 
+             * You can also use the ^ symbol in ranges. The following returns the last two characters:
+             */
+            char[] lastTwo = vowels[^2..]; // 'o', 'u'
+
+            Range firstTwoRange = 0..2;
+            char[] firstTwos = vowels[firstTwoRange]; // 'a', 'e'
+        }
     }
 }
