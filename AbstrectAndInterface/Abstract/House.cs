45fbae3a47e9b2e22b64aac7ae60af7fb18509d0
mkpbd <mostofakamal1990@gmail.com>
2022-07-20 18:52:19 +0600

    sele keyword

---
+++ AbstrectAndInterface/Abstract/House.cs
@@ -0,0 +1,17 @@
+﻿using CSharpInNutShell.Chipter3.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrectAndInterface.Abstract
+{
+    internal class House : Asset
+    {
+        public override decimal NetValue => throw new NotImplementedException();
+        public decimal Mortgage;
+        public   decimal Liability { get { return Mortgage; } }
+       // public sealed override decimal Liability { get { return Mortgage; } }
+    }
+}
